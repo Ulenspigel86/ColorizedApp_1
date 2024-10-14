9f257fa8bbc2e1aedf6411c5307ea7ad39b1026b
Ulenspigel86 <66313198+Ulenspigel86@users.noreply.github.com>
2024-10-14 18:15:31 +0300

    Final Version HW 2.04

--- ColorizedApp_1/ViewController.swift
+++ ColorizedApp_1/ViewController.swift
@@ -7,13 +7,60 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        @IBOutlet weak var colorView: UIView!
+        
+        @IBOutlet weak var redLabel: UILabel!
+        @IBOutlet weak var greenLabel: UILabel!
+        @IBOutlet weak var blueLabel: UILabel!
+    
+        @IBOutlet weak var redSlider: UISlider!
+        @IBOutlet weak var greenSlider: UISlider!
+        @IBOutlet weak var blueSlider: UISlider!
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            
+            colorView.layer.cornerRadius = 10
+            colorValueChanged()
+            
+            redLabel.text = string(from: redSlider)
+            greenLabel.text = string(from: greenSlider)
+            blueLabel.text = string(from: blueSlider)
+        }
+        
+        @IBAction func sliderValueChanged(_ sender: UISlider) {
+            colorValueChanged()
+            
+            switch sender {
+            case redSlider:
+                redLabel.text = string(from: redSlider)
+            case greenSlider:
+                greenLabel.text = string(from: greenSlider)
+            default:
+                blueLabel.text = string(from: blueSlider)
+            }
+        }
+        
+        private func colorValueChanged() {
+            colorView.backgroundColor = UIColor(
+                red: redSlider.value.cgColorValueChanged(),
+                green: greenSlider.value.cgColorValueChanged(),
+                blue: blueSlider.value.cgColorValueChanged(),
+                alpha: 1
+            )
+        }
+        
+        private func string(from slider: UISlider) -> String {
+            String(format: "%.2f", slider.value)
+        }
     }
 
+    extension Float {
+        func cgColorValueChanged() -> CGFloat {
+            CGFloat(self)
+        }
+    }
 
-}
 
